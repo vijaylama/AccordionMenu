4cdb5dc3e037be4ed038e36f6769a0a96200a32a
Vijay Lama <b_jay05@hotmail.com>
2019-08-27 17:26:32 -0400

    accordion menu desktop-version and mobile version completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'accordion-app';
+  isSmartPhone: boolean;
+  firstMenu: {} = "content.firstMenu.isClicked";
+  content = {
+    firstMenu: {
+      title: "First Title",
+      body:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, ab praesentium magnam similique voluptatibus omn voluptates dignissimos corporis illum doloremque.",
+      isClicked: false
+    },
+    secondMenu: {
+      title: "Second Title",
+      body:
+        "Porem, ipsum dolor sit amet consectetur adipisicing elit. Ut, ab praesentium magnam similique voluptatibus omn sdafsd dignissimos corporis illum doloremque.",
+      isClicked: false
+    },
+    thirdMenu: {
+      title: "Third Title",
+      body:
+        "sdaf, hdsfa dolor sit amet consectetur adipisicing elit. Ut, ab praesentium magnam similique voluptatibus omn sdafsd dignissimos corporis illum dfas.",
+      isClicked: false
+    }
+  };
+
+  //listen window resize
+  @HostListener("window:resize", ["$event"])
+  onresize(event): void {
+    this.isSmartPhone = event.target.innerWidth < 381;
+  }
+
+  onClick(title): void {
+    if (title === this.content.firstMenu.title) {
+      this.content.firstMenu.isClicked = !this.content.firstMenu.isClicked;
+      this.content.secondMenu.isClicked = false;
+      this.content.thirdMenu.isClicked = false;
+    }
+    if (title === this.content.secondMenu.title) {
+      this.content.secondMenu.isClicked = !this.content.secondMenu.isClicked;
+      this.content.firstMenu.isClicked = false;
+      this.content.thirdMenu.isClicked = false;
+    }
+    if (title === this.content.thirdMenu.title) {
+      this.content.thirdMenu.isClicked = !this.content.thirdMenu.isClicked;
+      this.content.firstMenu.isClicked = false;
+      this.content.secondMenu.isClicked = false;
+    }
+  }
+  ngOnInit(): void {
+    this.isSmartPhone = window.innerWidth < 381;
+  }
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Content } from "@angular/compiler/src/render3/r3_ast";
+
+@Component({
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"]
+})
+export class CardComponent implements OnInit {
+  @Input() content;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
